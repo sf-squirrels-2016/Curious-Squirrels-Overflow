77007d3b1da8f41819135db036a846128ffe9904
Timur Catakli <timurcatakli@gmail.com>
2016-02-11 16:58:52 -0800

    Add schema.xml, mvp pdf files

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_many :retweets
 
-  has_many :follower, class_name: "Connection", foreign_key: "follower_id"
   has_many :followed, through: :connections, class_name: "Connection", foreign_key: "followed_id"
+  has_many :followers, through: :connections, class_name: "Connection", foreign_key: "follower_id"
   # Remember to create a migration!
 end
