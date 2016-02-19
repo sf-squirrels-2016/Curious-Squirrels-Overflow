d6ed3688975a2bbafb829836712d97fe954b757d
Don Lang & Peter Lowe <pair+donlang.peterlowe42@devbootcamp.com>
2016-02-18 16:00:10 -0800

    Add routes for adding votes

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -10,3 +10,18 @@ get '/questions/:id' do
   erb :question
 end
 
+post "/questions/:id/votes/new" do
+  @question = Question.find(params[:id])
+
+  vote = Vote.create(votable_id: params[:id], votable_type: 'Question', up_vote: params[:up])
+
+  redirect "/questions/#{@question.id}"
+end
+
+post "/questions/:id/answers/:answer_id/votes/new" do
+  answer = Answer.find(params[:answer_id])
+
+  vote = Vote.create(votable_id: params[:answer_id], votable_type: 'Answer', up_vote: params[:up])
+
+  redirect "/questions/#{params[:id]}"
+end
